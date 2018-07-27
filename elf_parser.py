e1174f980aa04074f53a2c601e978b5f8ddd30c2
Mert Değirmenci <r00tten@Merts-MacBook-Air.local>
2018-07-27 03:13:42 +0300

    Section header parsing and string section parsing functionality has been added.

--- elf_parser.py
+++ elf_parser.py
@@ -5,11 +5,15 @@ from struct import *
 fileName = ''
 fileClassID = ''
 
-types = {0:'No file type', 1:'Relocatable object file', 2:'Executable file', 3:'Shared object file', 4:'Core file'}
+fileTypes = {0:'No file type', 1:'Relocatable object file', 2:'Executable file', 3:'Shared object file', 4:'Core file'}
 machines = {3:'Intel 80386', 62:'AMD x86-64 architecture'}
 versions = {0:'Invalid version', 1:'Current version'}
 datas = {1:'Little-endian', 2:'Big-endian'}
 classes = {1:'32-bit objects', 2:'64-bit objects'}
+
+sectionTypes = {0:'SHT_NULL', 1:'SHT_PROGBITS', 2:'SHT_SYMTAB', 3:'SHT_STRTAB', 4:'SHT_RELA', 5:'SHT_HASH', 6:'SHT_DYNAMIC', 7:'SHT_NOTE', 8:'SHT_NOBITS', 9:'SHT_REL', 10:'SHT_SHLIB', 11:'SHT_DYNSYM', 14:'SHT_INIT_ARRAY', 15:'SHT_FINI_ARRAY', 16:'SHT_PREINIT_ARRAY', 17:'SHT_GROUP', 18:'SHT_SYMTAB_SHNDX', 19:'SHT_NUM'}
+sectionFlags = {0:'', 1:'SHF_WRITE', 2:'SHF_ALLOC', 3:'SHF_EXECINSTR'}
+sectionInfos = {}
 
 class elfHeaderClass:
     e_indent = []
@@ -27,7 +31,20 @@ class elfHeaderClass:
     e_shnum = ''
     e_shstrndx = ''
 
+class sectionHeaderClass:
+    sh_name = ''
+    sh_type = ''
+    sh_flags = ''
+    sh_addr = ''
+    sh_offset = ''
+    sh_size = ''
+    sh_link = ''
+    sh_info = ''
+    sh_addralign = ''
+    sh_entsize = ''
+
 curElfHeader = elfHeaderClass()
+curSectionHeader = []
 
 def elfHeaderParser():
     global curElfHeader
@@ -51,7 +68,7 @@ def elfHeaderParser():
     chunk = file.read(2)
     offset += 2
     decimalValue = unpack('H', chunk)[0]
-    curElfHeader.e_type = types[decimalValue]
+    curElfHeader.e_type = fileTypes[decimalValue]
 
     chunk = file.read(2)
     offset += 2
@@ -158,6 +175,169 @@ def magicalCheck():
 
     return check
 
+def sectionHeaderParser():
+    if not curElfHeader:
+        exit()
+    
+    offset = 0
+    chunk = ''
+    decimalValue = 0
+    headerOffset = int(curElfHeader.e_shoff, 16)
+
+    entryCount = curElfHeader.e_shnum
+    entrySize = curElfHeader.e_shentsize
+
+    file = open(fileName, 'rb')
+    file.seek(headerOffset)
+
+    for i in range(entryCount):
+        curSectionHeader.append(sectionHeaderClass())
+        index = len(curSectionHeader) - 1
+
+        chunk = file.read(4)
+        offset += 4
+        decimalValue = unpack('I', chunk)[0]
+        curSectionHeader[index].sh_name = decimalValue
+
+        chunk = file.read(4)
+        offset += 4
+        decimalValue = unpack('I', chunk)[0]
+        if decimalValue <= 19:
+            curSectionHeader[index].sh_type = sectionTypes[decimalValue]
+        else:
+            curSectionHeader[index].sh_type = 'PROGBITS'
+
+        if fileClassID == 1:
+            chunk = file.read(4)
+            offset += 4
+            decimalValue = unpack('I', chunk)[0]
+            curSectionHeader[index].sh_flags = decimalValue
+
+            for i in range(4):
+                chunk = file.read(1)
+                offset += 1
+                curSectionHeader[index].sh_addr = chunk.encode('hex') + curSectionHeader[index].sh_addr
+            
+            curSectionHeader[index].sh_addr = '0x' + curSectionHeader[index].sh_addr
+            
+            for i in range(4):
+                chunk = file.read(1)
+                offset += 1
+                curSectionHeader[index].sh_offset = chunk.encode('hex') + curSectionHeader[index].sh_offset
+            
+            curSectionHeader[index].sh_offset = '0x' + curSectionHeader[index].sh_offset
+
+            chunk = file.read(4)
+            offset += 4
+            decimalValue = unpack('I', chunk)[0]
+            curSectionHeader[index].sh_size = decimalValue
+        else:
+            chunk = file.read(8)
+            offset += 8
+            decimalValue = unpack('Q', chunk)[0]
+            curSectionHeader[index].sh_flags = decimalValue
+
+            for i in range(8):
+                chunk = file.read(1)
+                offset += 1
+                curSectionHeader[index].sh_addr = chunk.encode('hex') + curSectionHeader[index].sh_addr
+            
+            curSectionHeader[index].sh_addr = '0x' + curSectionHeader[index].sh_addr
+
+            for i in range(8):
+                chunk = file.read(1)
+                offset += 1
+                curSectionHeader[index].sh_offset = chunk.encode('hex') + curSectionHeader[index].sh_offset
+            
+            curSectionHeader[index].sh_offset = '0x' + curSectionHeader[index].sh_offset
+
+            chunk = file.read(8)
+            offset += 8
+            decimalValue = unpack('Q', chunk)[0]
+            curSectionHeader[index].sh_size = decimalValue
+
+        for i in range(4):
+            chunk = file.read(1)
+            offset += 1
+            curSectionHeader[index].sh_link = chunk.encode('hex') + curSectionHeader[index].sh_link
+        
+        curSectionHeader[index].sh_link = '0x' + curSectionHeader[index].sh_link
+
+        chunk = file.read(4)
+        offset += 4
+        decimalValue = unpack('I', chunk)[0]
+        curSectionHeader[index].sh_info = decimalValue
+
+        if fileClassID == 1:
+            chunk = file.read(4)
+            offset += 4
+            decimalValue = unpack('I', chunk)[0]
+            curSectionHeader[index].sh_addralign = decimalValue
+
+            chunk = file.read(4)
+            offset += 4
+            decimalValue = unpack('I', chunk)[0]
+            curSectionHeader[index].sh_entsize = decimalValue
+        else:
+            chunk = file.read(8)
+            offset += 8
+            decimalValue = unpack('Q', chunk)[0]
+            curSectionHeader[index].sh_addralign = decimalValue
+
+            chunk = file.read(8)
+            offset += 8
+            decimalValue = unpack('Q', chunk)[0]
+            curSectionHeader[index].sh_entsize = decimalValue
+
+    if len(curSectionHeader) != curElfHeader.e_shnum or offset != (curElfHeader.e_shnum * curElfHeader.e_shentsize):
+        file.close()
+        exit()
+    else:
+        stringTableParser()
+    file.close()
+
+def stringTableParser():
+    offset = 0
+    tableSectionIndex = int(curElfHeader.e_shstrndx)
+    tableOffset = int(curSectionHeader[tableSectionIndex].sh_offset, 16)
+    size = int(curSectionHeader[tableSectionIndex].sh_size)
+    chunk = ''
+    decimalValue = 0
+    strValue = ''
+
+    file = open(fileName, 'rb')
+
+    for i in curSectionHeader:
+        strOffset = int(i.sh_name)
+        file.seek(tableOffset + strOffset)
+
+        while chunk != '\0':
+            chunk = file.read(1)
+            if(chunk != '\0'):
+                strValue += chunk
+        
+        i.sh_name = strValue
+        strValue = ''
+        chunk = ''
+
+
+    file.seek(tableOffset)
+
+"""     while offset < size:
+        chunk = file.read(1)
+        offset += 1
+        if chunk.encode('hex') == '\0':
+            while chunk != '\0':
+                chunk = file.read(1)
+                offset += 1
+                if chunk != '\0':
+                    strValue += unpack('c')[0]
+            curStringSection.append(strValue)
+            strValue = ''
+                    
+
+        curStringSection.append() """
+
 def elfHeaderPrinter():
     magicSeq = ''
     for i in range(4):
@@ -184,6 +364,19 @@ def elfHeaderPrinter():
     print ('{:<50}' + str(curElfHeader.e_shnum)).format('Number of section header entries:')
     print ('{:<50}' + str(curElfHeader.e_shstrndx)).format('Section name string table index:')
 
+def sectionHeaderPrinter():
+    index = 0
+
+    print 
+    print 'Section Header of {}:'.format(fileName)
+    print
+    print ('{0:<5}{1:<21}{2:<17}{3:<8}{4:<21}{5:<21}{6:<7}{7:<13}{8:<7}{9:<12}{10:<0}').format('No', 'Name', 'Type', 'Flags', 'Address', 'Offset', 'Size', 'Link', 'Info', 'Alignment', 'EntSize')
+    print ('{0:<5}{4:<21}{2:<17}{3:<8}{4:<21}{4:<21}{5:<7}{6:<13}{5:<7}{7:<12}{8:<0}').format('--', '--------', '--------------', '-----', '------------------', '----', '----------', '---------', '-------')
+
+    for i in curSectionHeader:
+        print ('{0:<5}{1:<21}{2:<17}{3:<8}{4:<21}{5:<21}{6:<7}{7:<13}{8:<7}{9:<12}{10:<13}').format(index, i.sh_name, i.sh_type, i.sh_flags, i.sh_addr, i.sh_offset, i.sh_size, i.sh_link, i.sh_info, i.sh_addralign, i.sh_entsize)
+        index += 1
+
 def sign():
     print
     print "             ___   ___  _   _             "
@@ -203,6 +396,7 @@ def main():
     argumentParser = argparse.ArgumentParser("Elf parser.")
     argumentParser.add_argument('file', nargs=1, metavar='elf file')
     argumentParser.add_argument('-e', '--elf-header', action='store_true')
+    argumentParser.add_argument('-s', '--section-header', action='store_true')
 
     if len(sys.argv) <= 1:
         sys.argv.append('--help')
@@ -214,6 +408,9 @@ def main():
 
     if len(sys.argv) == 2 or args.elf_header:
         elfHeaderPrinter()
+    if args.section_header:
+        sectionHeaderParser()
+        sectionHeaderPrinter()
 
 if __name__ == "__main__":
     main()
